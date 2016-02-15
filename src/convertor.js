03707541f4ea2cdd037cf646f7141e4cc9c93aaa
Jon Hall <jon_hall@outlook.com>
2016-02-15 20:19:26 +0000

    add convertor

---
+++ src/convertor.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const map = {},
+    COMBINING_DIACRITICALS_START = parseInt('0300', 16);
+
+let get_code = String.fromCharCode.bind(String),
+    adjustment = 0;
+
+for(let i = 0, limit = 111; i < limit; i++) {
+    // Create a map in both directions
+    map[map[get_code(i + adjustment)] = get_code(i + COMBINING_DIACRITICALS_START)] = get_code(i + adjustment);
+    if(i === 15) {
+        // Skip from 15 to 32, since we only have 112 combining chars to use (no useful js chars in this range)
+        adjustment = 16;
+    }
+}
+
+module.exports = function (str) {
+    return [].map.call(str, char => map[char]).join('');
+};
--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,28 @@
 'use strict';
+const one = require('../index.js'),
+    // http://www.unicode.org/charts/PDF/U0300.pdf
+    COMBINING_DIACRITICALS_START = parseInt('0300', 16),
+    COMBINING_DIACRITICALS_END = parseInt('036F', 16);
 
 describe('onespace', function() {
-    it('is', function() {
-        expect(' '.length).toBe(1);
+    function is_one_space(str, should_fail) {
+        [].forEach.call(str, char => {
+            expect(char.charCodeAt(0) <= COMBINING_DIACRITICALS_END &&
+                char.charCodeAt(0) >= COMBINING_DIACRITICALS_START).toBe(!should_fail);
+        });
+    }
+
+    it('converts', function() {
+        is_one_space(one.convert('this is not one space'));
+    });
+
+    it('converts back', function() {
+        let original = 'this is not one space',
+            converted = one.convert(original);
+
+        is_one_space(converted);
+        is_one_space(original, true);
+
+        expect(one.convert(converted)).toBe(original);
     });
 });
